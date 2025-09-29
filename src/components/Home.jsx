3b16add46c5dadbb9b1a9e8964575c6558401fa0
JuliaBonartDev <juliabonartdev@gmail.com>
2025-09-29 20:24:43 +0200

    Slider's static image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="home">
       <h1>Proyecto limpio 🚀</h1>
       <p>Ya no dependemos de App.jsx</p>
+      <img src="/src/assets/images/cat-551554_1280.jpg" className="imgSlider" />
     </div>
   );
 }

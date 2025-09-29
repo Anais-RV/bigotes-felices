1f652e29d29ea54460cdb14019c6e801d47cb731
JuliaBonartDev <juliabonartdev@gmail.com>
2025-09-29 12:39:58 +0200

    chore: clean react Project

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Proyecto limpio 🚀</h1>
+      <p>Ya no dependemos de App.jsx</p>
+    </div>
+  );
+}

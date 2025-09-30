83b51a4c083a6f7e60a76ed329a19d43e7ae344a
Iván Calero <ivanicaleroo@gmail.com>
2025-09-30 11:26:31 +0200

    Header with navbar links and project structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="navbar">
+      <div className="navbar-content">
+        <div className="slider-placeholder">Aquí irá el slider</div>
+        <div className="modal-box">Modal con contenido del slider</div>
+
+        <nav className="nav-actions">
+          <NavLink to="/" className="nav-link">HOME</NavLink>
+          <NavLink to="/favorites" className="nav-link">FAVORITOS</NavLink>
+          <NavLink to="/adopt" className="nav-link">ADOPTAR</NavLink>
+        </nav>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './components/Home'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

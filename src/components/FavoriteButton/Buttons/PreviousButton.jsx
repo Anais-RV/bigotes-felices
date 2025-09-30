eb339b803af81f602a9edcec2e9347c02c30e2f4
miguel-angel-git <miguelangelgit@gmail.com>
2025-09-30 11:38:38 +0200

    HOME-05 - implemented Previous and next buttons

---
+++ src/components/FavoriteButton/Buttons/PreviousButton.jsx
@@ -0,0 +1,11 @@
+// BotónSiguiente.jsx
+import React from 'react';
+
+export default function PreviousButton({ prev }) {
+    return (
+
+        <>
+            <button onClick={prev}>←</button>
+        </>
+    );
+}

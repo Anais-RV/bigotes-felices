d38fd7fe410c76bbd59bd6126277a9d4504ab1ed
Antonio J. Cabrera <157977235+ajc2005@users.noreply.github.com>
2025-09-30 19:08:45 +0200

    Created router, pending for revision (#20)
    * Created router, pending for revision
    
    * fix/ROUTE-01: duplicated home - add alias - export
    
    ---------
    
    Co-authored-by: Anaïs Rodríguez Villanueva <119762586+Anais-RV@users.noreply.github.com>

---
+++ src/router/router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import HomePage from "../pages/Home/HomePage.jsx";
+import AdoptPage from "../pages/Adopt/AdoptPage.jsx";
+import FavoritesPage from "../pages/Favorites/FavoritesPage.jsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/home",              // 👈 alias
+    element: <Navigate to="/" replace />
+  },
+  {
+    path: "/adopt",
+    element: <AdoptPage />
+  },
+  {
+    path: "/favorites",
+    element: <FavoritesPage />
+  }
+]);

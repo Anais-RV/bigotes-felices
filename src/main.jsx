d7651eb52cc1f1c5f957efaaf2ad7af61fbe8e4b
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-01 11:23:48 +0200

    wrap main.jsx with the provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './components/Home'
-
+import { FavoritesProvider } from './context/FavoritesContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <FavoritesProvider>
+      <Home />
+    </FavoritesProvider>
   </StrictMode>,
 )

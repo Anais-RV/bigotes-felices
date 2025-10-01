8f3b3aa264084f51c4a25714c223ce918efd592b
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-01 09:41:47 +0200

    Actions called

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 
+
 export default function Home() {
   return (
     <div className="home">
       <h1>Proyecto limpio 🚀</h1>
       <p>Ya no dependemos de App.jsx</p>
+
     </div>
   );
 }
---
+++ src/context/FavoritesContext.jsx
@@ -0,0 +1,5 @@
+import React, { createContext, useContext, useReducer} from "react";
+
+//Acciones
+const ADD_FAVORITE = "ADD_FAVORITE";
+const REMOVE_FAVORITE = "REMOVE_FAVORITE";

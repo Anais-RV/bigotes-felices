8620f29737d3e91e614e5017b9e2a767a899e0f2
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-01 10:01:21 +0200

    create function reducer & context favorites

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -3,3 +3,36 @@ import React, { createContext, useContext, useReducer} from "react";
 //Acciones
 const ADD_FAVORITE = "ADD_FAVORITE";
 const REMOVE_FAVORITE = "REMOVE_FAVORITE";
+
+//Estado inicial con un objeto 
+const inicial = {
+    favorites: [],
+};
+
+//Reducer
+const favoritesReducer = (state, action) => {
+    switch(action.type) {
+        case ADD_FAVORITE:
+            //Evitar duplicados
+            if (state.favorites.some(item => item.id === action.payload.id)) {
+                return state;
+            }
+
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload],
+            };
+
+        case REMOVE_FAVORITE:
+            return {
+                ...state,
+                favorites: state.favorites.filter(item => item.id !== action.payload.id),
+            }
+        
+        default:
+            return state;
+    }
+};
+
+//Crear contexto
+const FavoritesContext = createContext();

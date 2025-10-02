8a5e913f476464f7eb10c10f2f891d3a45e58af9
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-02 17:49:25 +0200

    Feat/fav 01 config reducer (#42)
    * update project 00
    
    * update project 01 create favoritescontext
    
    * update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+{/**Importamos FavoritesProvider */}
+import { FavoritesProvider } from "./context/FavoritesContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    {/**Englobamos FavoritesProvider */}
+    <FavoritesProvider>
+      {/**Componentes */}
+      <RouterProvider router={router} />
+    </FavoritesProvider>
+    
   </StrictMode>
 );

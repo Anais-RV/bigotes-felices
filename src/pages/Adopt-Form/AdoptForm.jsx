6fd392c900d52d69b09ab9d096a7dea54c49f682
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-02 13:31:53 +0200

    create inputs AdoptForm.jsx upd

---
+++ src/pages/Adopt-Form/AdoptForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const AdoptForm = () => {
+  return (
+    <form action="#" method='post'>
+        <h1>Formulario de adopción</h1>
+        <fieldset>
+            <legend><label for="fullname">Nombre completo:</label></legend>
+            <input type='text' id='fullname' className='fullname' placeholder='Juan Cachopo' />
+        </fieldset>
+        <fieldset>
+            <legend><label for="email">Correo electrónico: </label></legend>
+            <input type='email' id='email' className="email" placeholder="juancachopo@gmail.com" />
+        </fieldset>
+        <fieldset>
+            <legend><label for="phone">Teléfono: </label></legend>
+            <input type='text' id='phone' className='phone' placeholder='985123123' />
+        </fieldset>
+        <fieldset>
+            <legend><label for="message">Mensaje: </label></legend>
+            <textarea id='message' className='message' placeholder='Danos su opinión...'></textarea>
+        </fieldset>
+        <button type='submit' id="submit" className="submit">
+            Enviar
+        </button>
+        <button type='reset' id="reset" name="reset">
+            Reiniciar
+        </button> 
+    </form>
+  )
+}
+
+export default AdoptForm

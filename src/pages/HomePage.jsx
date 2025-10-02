5b8d3c54822cb83be981deae7c7e1cb26b14bbc4
wizisergio <wizisergio@gmail.com>
2025-10-02 12:20:00 +0200

    Done HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home">
+      <h1>Proyecto limpio 🚀</h1>
+      <p>Ya no dependemos de App.jsx</p>
+    </div>
+  );
+};
+
+export default HomePage;

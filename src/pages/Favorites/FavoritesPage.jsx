0fc03926875bd3349c1fb40bcea557bb1df268bc
Antonio J. <antonio.j.cabrera2005@gmail.com>
2025-10-02 12:13:33 +0200

    favPage structure done, cards pending

--- src/pages/Favorites/FavoritesPage.jsx
+++ src/pages/Favorites/FavoritesPage.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import { readAllCats } from "../../service/catService";
+import { useEffect } from "react";
+
 const FavoritesPage = () => {
-  return <p>Favorites Page</p>;
-};
+  
+  const [favCards, setFavCards] = useState([]);
+
+  const getAllCatsFromService = async () => {
+    const DATA = await readAllCats();
+    setFavCards(DATA);
+  };
+
+  useEffect(() => { getAllCatsFromService(); }, []);
+
+  return (
+    <>
+      {/* HEADER */}
+      <main>
+        <section className="main__section--fav-cards">
+          {favCards.map((favCard) => (
+            <div key={favCard.id}></div> // Card Component inside here
+          ))}
+        </section>
+        <div className="div__btn--styling">
+          <button className="btn__btn--state-adopt-all">Adoptar</button>
+          <Link to="/">
+            <button className="btn__btn--state-home-link">Home</button>
+          </Link>
+        </div>
+      </main>
+    </>
+  )
+}
 
 export default FavoritesPage;

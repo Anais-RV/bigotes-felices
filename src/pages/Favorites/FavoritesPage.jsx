78460677225887ff94790db205a6ac8bab15f0d9
Antonio J. <antonio.j.cabrera2005@gmail.com>
2025-10-02 13:36:57 +0200

    fixed useEffect import

--- src/pages/Favorites/FavoritesPage.jsx
+++ src/pages/Favorites/FavoritesPage.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { readAllCats } from "../../service/catService";
-import { useEffect } from "react";
 
 const FavoritesPage = () => {
   

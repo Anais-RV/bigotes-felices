f6c31857e47c2ea0793a68297fbc62cba0af2d1d
WiZiSergio <wizisergio@gmail.com>
2025-10-07 09:28:40 +0200

    Fix PR salchipapa FAV-03

--- src/pages/Favorites/FavoritesPage.jsx
+++ src/pages/Favorites/FavoritesPage.jsx
@@ -1,5 +1,34 @@
+import { Link } from 'react-router-dom';
+import { useFavorites } from '../../context/FavoritesContext';
+import CatCard from '../../components/CatCard/CatCard';
+
 const FavoritesPage = () => {
-  return <p>Favorites Page</p>;
+  const { favoriteCats } = useFavorites();
+
+  return (
+    <>
+      <main>
+        <section className="main__section--fav-cards">
+          {favoriteCats.length === 0 ? (
+            <div className="empty-favorites">
+              <h2>No tienes favoritos</h2>
+              <p>¡Explora nuestros gatitos y añade algunos a tus favoritos!</p>
+            </div>
+          ) : (
+            favoriteCats.map((cat) => (
+              <CatCard key={cat.id} cat={cat} />
+            ))
+          )}
+        </section>
+        <div className="div__btn--styling">
+          <button className="btn__btn--state-adopt-all">Adoptar</button>
+          <Link to="/">
+            <button className="btn__btn--state-home-link">Home</button>
+          </Link>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default FavoritesPage;

96e889be5a6d2b557c263568dc74b8b400988946
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-10-08 14:13:20 +0200

    add const loading, error & data

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,8 +1,14 @@
 import Header from '../../components/Header/Header';
 import Slider from '../../Slider/Slider';
 import CatCard from '../../components/CatCard/CatCard';
+import { useState, useEffect } from 'react';
 
 const HomePage = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
   return (
     <Header 
       slider={<Slider />}

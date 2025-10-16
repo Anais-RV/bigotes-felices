4e42b86d0531c6d62426f893d5da01d770f7dfc5
Anaïs Rodríguez Villanueva <119762586+Anais-RV@users.noreply.github.com>
2025-10-16 10:21:13 +0200

    feat(favorites):  fix translations

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { FavoritesProvider } from './context/FavoritesContext';
+import { LanguageProvider } from './context/LanguageContext';
 import './index.css';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FavoritesProvider>
-      <App />
-    </FavoritesProvider>
+    <LanguageProvider>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
+    </LanguageProvider>
   </StrictMode>
 );
